f8d4e61e0f357b420949bd6018bf1beaa0747c8e
CutePenguins11111 <105313048+CutePenguins11111@users.noreply.github.com>
2022-05-17 10:36:08 -0700

    Update pengins.py

--- pengins.py
+++ pengins.py
@@ -5,4 +5,24 @@ tim = tello.Tello()
 tim.connect()
 print(tim.get_battery())
 
+# taking off and moving
 
+tim.takeoff()
+tim.move_up(120)
+tim.move_forward(400)
+tim.move_forward(400)
+
+# turning
+
+tim.rotate_counter_clockwise(90)
+tim.move_forward(400)
+tim.rotate_counter_clockwise(90)
+
+# Final half
+
+tim.move_forward(400)
+tim.move_forward(400)
+tim.rotate_counter_clockwise(90)
+tim.move_forward(400)
+tim.move_down(120)
+tim.land()
